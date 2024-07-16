452d30d6b60017cdfd98e2839b56ad2bb4ef4883
fm1psy <freddymateo1@gmail.com>
2024-07-16 15:19:33 +0100

    add title and pages to main page

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -1,9 +1,20 @@
 import streamlit as st
+from st_pages import Page, show_pages
 
 
 def deploy_home_page():
     """This displays the main page of the dashboard."""
     st.set_page_config(layout="wide")
+
+    show_pages(
+        [
+            Page("main_page.py", "Home", "🏠"),
+            Page("pages/daily_report.py",
+                 "Summary Report", "📄"),
+            Page("pages/incident_alerts.py",
+                 "Train Alerts", "⚠️")
+        ]
+    )
     st.title("Railway Tracker 🚆")
 
 
