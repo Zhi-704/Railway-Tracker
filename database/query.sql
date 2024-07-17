7b121b5c1cae47686a1d8a71344ae0f14caed94f
Fariha <trainee.fariha.choudhury@sigmalabs.co.uk>
2024-07-17 16:12:17 +0100

    remove comments

--- database/query.sql
+++ database/query.sql
@@ -1,19 +1,12 @@
 -- A file to query the database
 
--- SELECT incident_number, operator_code, creation_time FROM incident;
--- SELECT * FROM cancellation;
--- SELECT * FROM station_performance_archive;
--- SELECT * FROM users;
--- SELECT * FROM cancel_code;
--- SELECT * FROM station;
--- SELECT * FROM subscription;
--- SELECT * FROM waypoint;
--- SELECT * FROM operator;
+SELECT incident_number, operator_code, creation_time FROM incident;
+SELECT * FROM cancellation;
+SELECT * FROM station_performance_archive;
+SELECT * FROM users;
+SELECT * FROM cancel_code;
+SELECT * FROM station;
+SELECT * FROM subscription;
+SELECT * FROM waypoint;
+SELECT * FROM operator;
 
-
-SELECT incident_number, creation_time FROM incident;
-SELECT * FROM affected_operator;
-
-
-DELETE FROM affected_operator;
-DELETE FROM incident;
