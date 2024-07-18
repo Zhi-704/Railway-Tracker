d280b46b788479a2ec407e09f5fa373b0f53cadc
Zhi-704 <zhi.liang.704@gmail.com>
2024-07-18 10:25:30 +0100

    Service, operator, station functionality working

--- realtime_trains/load.py
+++ realtime_trains/load.py
@@ -15,6 +15,8 @@ REQUIRED_FIELDS_SERVICE_LOCATION_DETAILS = ["displayAs", "tiploc", "crs"]
 REQUIRED_FIELDS_SERVICE = ["locationDetail",
                            "serviceUid", "atocCode", "atocName"]
 REQUIRED_FIELDS_LOCATION = ["name", "crs"]
+CANCELLATION_FIELDS = ["cancelReasonCode",
+                       "cancelReasonShortText", "cancelReasonLongText"]
 
 
 def get_connection() -> DBConnection:
@@ -33,13 +35,100 @@ def get_cursor(conn: DBConnection) -> DBCursor:
     return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
-def get_id_if_exists(cur: DBCursor, table_name: str, conditions: dict) -> bool:
-    '''Checks if a certain data value already exists inside a table'''
-    query = f'''SELECT {table_name}_id FROM {table_name} WHERE {
-        ' AND '.join([f'{key} = %s' for key in conditions.keys()])}'''
+def get_id_if_exists(cur: DBCursor, table_name: str, conditions: dict[any]) -> int | None:
+    '''Checks if a certain data value already exists inside a table and retrieves their id'''
+    query = f'''SELECT {table_name}_id FROM {table_name} \
+        WHERE {' AND '.join([f'{key} = %s' for key in conditions.keys()])}'''
     cur.execute(query, tuple(conditions.values()))
-    id = cur.fetchone()
-    return id[0] if id is not None else None
+    table_id = cur.fetchone()
+    return table_id[0] if table_id is not None else None
+
+
+def insert_or_get_cancellation(cancelled_service: dict, waypoint_id: int, conn: DBConnection, cur: DBCursor):
+    '''Inserts a cancelled journey into the database'''
+    ...
+
+
+def insert_or_get_waypoint(station_id: int, service: dict, conn: DBConnection, cur: DBCursor):
+    '''Inserts or gets a journey from the database'''
+    waypoint_conditions = {
+        'station_id': station_id,
+
+    }
+
+    waypoint_id = get_id_if_exists(cur, "station", waypoint_conditions)
+
+    for key in service:
+        if key in CANCELLATION_FIELDS:
+            insert_or_get_cancellation(service, waypoint_id, conn, cur)
+
+
+def insert_or_get_service(service_dict: dict, operator_id: int, conn: DBConnection, cur: DBCursor) -> int:
+    '''Insert or get operator id from the database'''
+    service_conditions = {
+        'service_uid': service_dict["serviceUid"]
+    }
+
+    service_id = get_id_if_exists(cur, "service", service_conditions)
+    if service_id is None:
+        query = '''
+        INSERT INTO service (operator_id, service_uid)
+        VALUES
+        (%s, %s)
+        RETURNING service_id
+        '''
+
+        try:
+            cur.execute(query, (
+                operator_id,
+                service_dict["serviceUid"]
+            ))
+            service_id = cur.fetchone()
+            logging.info("Service %s added!", service_dict["serviceUid"])
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            logging.error(
+                "Load: Error occurred inserting Service: %s", e)
+            service_id = None
+    else:
+        logging.info("Service %s retrieved!", service_dict["serviceUid"])
+
+    return service_id
+
+
+def insert_or_get_operator(service_dict: dict, conn: DBConnection, cur: DBCursor) -> int:
+    '''Insert or get operator id from the database'''
+    operator_conditions = {
+        'operator_code': service_dict["atocCode"]
+    }
+
+    operator_id = get_id_if_exists(cur, "operator", operator_conditions)
+    if operator_id is None:
+        query = '''
+        INSERT INTO operator (operator_code, operator_name)
+        VALUES
+        (%s, %s)
+        RETURNING operator_id
+        '''
+
+        try:
+            cur.execute(query, (
+                service_dict["atocCode"],
+                service_dict["atocName"]
+            ))
+            operator_id = cur.fetchone()
+            logging.info("Operator %s added!", service_dict["crs"])
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            logging.error(
+                "Load: Error occurred inserting Operator: %s", e)
+            operator_id = None
+    else:
+        logging.info("Operator %s retrieved!", service_dict["atocCode"])
+
+    return operator_id
 
 
 def insert_or_get_station(location_dict: dict, conn: DBConnection, cur: DBCursor) -> int:
@@ -83,6 +172,9 @@ def import_to_database(stations: list[dict]) -> None:
 
     for station in stations:
         station_id = insert_or_get_station(station["location"], conn, cur)
+        for service in station["services"]:
+            operator_id = insert_or_get_operator(service, conn, cur)
+            service_id = insert_or_get_service(service, operator_id, conn, cur)
 
     cur.close()
     conn.close()
@@ -94,18 +186,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s - %(levelname)s - %(message)s")
     load_dotenv()
-    data = [get_realtime_trains_data("LDS"), get_realtime_trains_data("LST")]
+    data = [get_realtime_trains_data("LDS")]
     modified_data = process_all_stations(data)
     import_to_database(modified_data)
-
-    # location_dict = {
-    #     "name": "The Dreaded Station of the Pizza Man",
-    #     "crs": "PIZ"
-    # }
-    # conn = get_connection()
-    # cur = get_cursor(conn)
-
-    # print(insert_or_get_station(location_dict, conn, cur))
-
-    # cur.close()
-    # conn.close()
