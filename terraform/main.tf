151ef3d3b9ca1a0afad8e6f41c5a85c578f7f13e
Fariha <trainee.fariha.choudhury@sigmalabs.co.uk>
2024-07-18 16:08:49 +0100

    terraform lambda and event bridge

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,22 +7,22 @@ provider "aws" {
     secret_key = var.AWS_SECRET_KEY
 }
 
-# RDS: 
-resource "aws_db_instance" "c11-railway-tracker-db" {
-    allocated_storage            = 10
-    db_name                      = "railwaytrackerdb"
-    identifier                   = "c11-railway-tracker-db"
-    engine                       = "postgres"
-    engine_version               = "16.1"
-    instance_class               = "db.t3.micro"
-    publicly_accessible          = true
-    performance_insights_enabled = false
-    skip_final_snapshot          = true
-    db_subnet_group_name         = "c11-public-subnet-group"
-    vpc_security_group_ids       = [aws_security_group.c11-railway-tracker-RDS-sg-terrafrom.id]
-    username                     = var.DB_USERNAME
-    password                     = var.DB_PASSWORD
-}
+# # RDS: 
+# resource "aws_db_instance" "c11-railway-tracker-db" {
+#     allocated_storage            = 10
+#     db_name                      = "railwaytrackerdb"
+#     identifier                   = "c11-railway-tracker-db"
+#     engine                       = "postgres"
+#     engine_version               = "16.1"
+#     instance_class               = "db.t3.micro"
+#     publicly_accessible          = true
+#     performance_insights_enabled = false
+#     skip_final_snapshot          = true
+#     db_subnet_group_name         = "c11-public-subnet-group"
+#     vpc_security_group_ids       = [aws_security_group.c11-railway-tracker-RDS-sg-terrafrom.id]
+#     username                     = var.DB_USERNAME
+#     password                     = var.DB_PASSWORD
+# }
 
 resource "aws_security_group" "c11-railway-tracker-RDS-sg-terrafrom" {
     name = "c11-railway-tracker-RDS-sg-terrafrom"
@@ -47,3 +47,76 @@ resource "aws_security_group" "c11-railway-tracker-RDS-sg-terrafrom" {
 data "aws_vpc" "c11-vpc" {
     id = var.C11_VPC
 }
+
+
+
+
+# ARCHIVE PROCESS: LAMBDA 
+
+data "aws_iam_policy_document" "c11-railway-tracker-archive-lambda-policy-document" {
+    statement {
+        actions    = ["sts:AssumeRole"]
+        effect     = "Allow"
+        principals {
+            type        = "Service"
+            identifiers = ["lambda.amazonaws.com"]
+        }
+  }
+}
+
+resource "aws_iam_role" "c11-railway-tracker-archive-lambda-role" {
+  name               = "c11-railway-tracker-archive-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.c11-railway-tracker-archive-lambda-policy-document.json
+}
+
+
+resource "aws_lambda_function" "c11-railway-tracker-archive-lambda-function" {
+  role          = aws_iam_role.c11-railway-tracker-archive-lambda-role.arn
+  function_name = "c11-railway-tracker-archive-lambda-function"
+  package_type  = "Image"
+  architectures = ["x86_64"]
+  image_uri     = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c11-railway-tracker-archive-lambda-ecr:latest"
+    #   data.aws_ecr_image.archive-lambda-image.image_uri
+
+  environment {
+    variables = {
+      ACCESS_KEY_ID     = var.AWS_ACCESS_KEY,
+      SECRET_ACCESS_KEY = var.AWS_SECRET_KEY,
+      DB_HOST           = var.DB_IP,
+      DB_NAME           = var.DB_NAME,
+      DB_USER           = var.DB_USERNAME,
+      DB_PASS           = var.DB_PASSWORD,
+      DB_PORT           = var.DB_PORT,
+    }
+  }
+}
+
+# ARCHIVE EVENT BRIDGE SCHEDULE:
+    # - policy document ^^
+    # iam role
+    # event bridge schedule lambda at 9:30
+
+resource "aws_iam_role" "c11-railway-tracker-archive-schedule-role" {
+  name               = "c11-railway-tracker-archive-schedule-role"
+  assume_role_policy = data.aws_iam_policy_document.c11-railway-tracker-archive-lambda-policy-document.json
+}
+
+resource "aws_scheduler_schedule" "c11-railway-tracker-archive-schedule" {
+    # schedule archive process 9 am everyday
+  name = "c11-railway-tracker-archive-schedule"
+  group_name = "default"
+
+  schedule_expression = "cron(*/2 * * * *)"
+#   "cron(0 9 * * ? *)"
+  schedule_expression_timezone = "Europe/London"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn = aws_lambda_function.c11-railway-tracker-archive-lambda-function.arn
+    role_arn = aws_iam_role.c11-railway-tracker-archive-schedule-role.arn
+  }
+}
+
