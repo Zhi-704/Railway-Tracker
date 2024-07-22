2f60c5730b0f46feecc6dfe9b02601843f228477
fm1psy <freddymateo1@gmail.com>
2024-07-22 22:09:27 +0100

    add subscription form for incident alerts

--- dashboard/pages/incident_alerts.py
+++ dashboard/pages/incident_alerts.py
@@ -5,12 +5,14 @@ that notifies users if an operator they are interested in is suffering from an i
 import re
 from os import environ
 
+from boto3 import client
+from dotenv import load_dotenv
 import streamlit as st
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection, cursor
 
-def get_db_connection() -> connection:
+def get_db_connection() -> connection | None:
     """return a database connection"""
     try:
         return connect(
@@ -25,7 +27,7 @@ def get_db_connection() -> connection:
         return None
 
 
-def get_db_cursor(conn: connection) -> cursor:
+def get_db_cursor(conn: connection) -> cursor | None:
     """return a cursor object based on a given connection"""
     try:
         return conn.cursor(cursor_factory=RealDictCursor)
@@ -33,6 +35,32 @@ def get_db_cursor(conn: connection) -> cursor:
         st.write(e)
         return None
     
+def get_sns_client():
+    """return an AWS SNS client using access credentials, which are environment variables."""
+    try:
+        return client(
+            'sns',
+            aws_access_key_id=environ['AWS_ACCESS_KEY'],
+            aws_secret_access_key=environ['AWS_SECRET_KEY'],
+            )
+    except Exception as e: # pylint: disable=broad-exception-caught
+        st.write(e)
+        return None
+
+def get_user_contacts_from_form() -> dict | None:
+    """create a subscription form for users to submit their contact information."""
+    with st.form("alert_subscription", clear_on_submit=True, border=True):
+
+
+        operator = st.selectbox("Select an Operator", ("One", "Two", "Three"))
+
+        email = st.text_input("Enter your email")
+        phone_no = st.text_input("Enter your phone number")
+
+        submit = st.form_submit_button()
+
+        if operator and submit and (email or phone_no):
+            pass
 
 def deploy_alerts_page():
     """This serves as the main code for the alerts page"""
@@ -41,7 +69,10 @@ def deploy_alerts_page():
              and you will receive a notification when this operator
              is affected by an incident.""")
     
+    subscriber_inputs = get_user_contacts_from_form()
+    
 
 
 if __name__ == "__main__":
+    load_dotenv()
     deploy_alerts_page()
