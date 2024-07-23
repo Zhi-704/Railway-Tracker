b6bb103c96af6d0cafd9511fffbb7905b9bdadfc
Zhi-704 <zhi.liang.704@gmail.com>
2024-07-23 11:26:25 +0100

    Email notification done, need to figure out sms

--- national_rail/sns_reporting.py
+++ national_rail/sns_reporting.py
@@ -1,16 +1,26 @@
 '''Script used for notifying all subscribed users of incidents for their operators.'''
 
 
+import json  # testing purposes
 from os import environ as ENV
 import logging
-from dotenv import load_dotenv
+from datetime import datetime
 
+from dotenv import load_dotenv
 from boto3 import client
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection, cursor
 from psycopg2 import connect
 
 FILTER_TOPICS_BY = "c11-trainwreck-"
+
+
+def get_data_from_json() -> list[dict]:  # testing purposes
+    with open('incidents.json', 'r') as file:
+        data = json.load(file)
+
+    return data
 
 
 def get_connection() -> connection:
@@ -28,51 +38,6 @@ def get_connection() -> connection:
 def get_cursor(conn: connection) -> cursor:
     """ Retrieves cursor and returns it. """
     return conn.cursor(cursor_factory=RealDictCursor)
-
-
-def get_latest_incidents(conn: connection,
-                         item_type: str,
-                         tag: str,
-                         sales_limit: int,
-                         sales_timeframe: str) -> list[dict]:
-    '''Gets trending sales data'''
-
-    query = f'''
-SELECT
-    T.{item_type}_id,
-    T.title,
-    A.name AS band,
-    COUNT(DISTINCT TP.{item_type}_purchase_id) AS copies_sold,
-    T.url
-FROM {item_type}_purchase AS TP
-JOIN {item_type} AS T ON TP.{item_type}_id = T.{item_type}_id
-JOIN artist AS A ON T.artist_id = A.artist_id
-JOIN {item_type}_tag_assignment AS TTA ON T.{item_type}_id = TTA.{item_type}_id
-JOIN tag AS TG ON TTA.tag_id = TG.tag_id
-WHERE TG.name = %s
-AND TP.timestamp >= NOW() - INTERVAL '{sales_timeframe}'
-GROUP BY
-    T.{item_type}_id,
-    T.title,
-    A.name,
-    T.url
-HAVING COUNT(DISTINCT TP.{item_type}_purchase_id) >= %s
-ORDER BY copies_sold DESC
-'''
-
-    try:
-        with conn.cursor() as cur:
-            cur.execute(query, (tag, sales_limit))
-            trending = cur.fetchall()
-
-        if len(trending) >= 1:
-            return trending
-    except Exception as e:
-        logging.error(
-            "Error fetching sales data for %s and tag %s: %s", item_type, tag, e)
-        conn.rollback()
-
-    return []
 
 
 def get_sns_client() -> client:
@@ -97,21 +62,96 @@ def filter_topics(filter_word: str, topic_list: list[dict]) -> list[dict]:
 
 
 def extract_oeprator_code(topic_arn: str) -> str:
-    '''Extracts the tag name from the topic arn'''
+    '''Extracts the operator code from the topic arn'''
     if topic_arn is None or not isinstance(topic_arn, str):
         return None
-    raw_tag = topic_arn.split(':')[-1]
-    tag_parts = raw_tag.split('-')[2:]
-    return ' '.join(tag_parts)
+    raw_str = topic_arn.split(':')[-1]
+    parts = raw_str.split('-')[2:]
+    return ' '.join(parts)
 
 
-def extract_operator_to_dictionary(tags_list: list[dict]) -> list[dict]:
+def extract_operator_to_dictionary(ops_list: list[dict]) -> list[dict]:
     '''Grabs the operator from list of arns'''
-    for tag_arn in tags_list:
-        topic_arn = tag_arn['TopicArn']
-        tag_name = extract_oeprator_code(topic_arn)
-        tag_arn['operator'] = tag_name
-    return tags_list
+    for op_arn in ops_list:
+        topic_arn = op_arn['TopicArn']
+        op_code = extract_oeprator_code(topic_arn)
+        op_arn['shortcode'] = op_code
+    return ops_list
+
+
+def find_common_elements(list1: list, list2: list) -> list:
+    """
+    Finds common elements between two lists and 
+    returns a list of elements that are present in both lists.
+    """
+    set_list2 = set(list2)
+    return [element for element in list1 if element in set_list2]
+
+
+def transform_datetime_string(input_string: str) -> str | None:
+    """
+    Transforms a datetime string from 'YYYY-MM-DDTHH:MM:SS.SSS+HH:MM' format
+    to 'YYYY-MM-DD HH:MM' format.
+    """
+    try:
+        dt = datetime.strptime(input_string, "%Y-%m-%dT%H:%M:%S.%f%z")
+        output_string = dt.strftime("%Y-%m-%d %H:%M")
+        return output_string
+    except:
+        return None
+
+
+def publish_list_to_topic(sns_client: client,
+                          topic_arn: str,
+                          operator_code: str,
+                          incidents_list: list[dict]) -> dict | None:
+    '''Publishes incidents for an operator, which sends a message to all subscribers'''
+
+    subject = f"New incident for {operator_code}"
+    message = f'''The following incident has been reported for {
+        operator_code}:\n\n'''
+    for incident in incidents_list:
+        if not all(key in incident for key in ['summary', 'start_time',
+                                               'end_time', 'uri',
+                                               'routes_affected']):
+            continue
+
+        incident_summary = incident['summary']
+        incident_start = transform_datetime_string(incident['start_time'])
+        incident_end = transform_datetime_string(incident['end_time'])
+        incident_routes = incident['routes_affected']
+        incident_uri = incident['uri']
+
+        message += f'''
+Routes Affected: {incident_routes}\n
+Start Date: {incident_start}\n
+End Date: {incident_end}\n
+Summary: {incident_summary}\n
+'''
+        message += f"Link to the above incident: {incident_uri}\n\n"
+
+    try:
+        response = sns_client.publish(
+            TopicArn=topic_arn,
+            Subject=subject,
+            Message=message,
+        )
+        return response
+    except NoCredentialsError:
+        logging.error("Error: No AWS credentials found.")
+    except PartialCredentialsError:
+        logging.error("Error: Incomplete AWS credentials.")
+    except Exception as e:
+        logging.error("An error occurred: %s", e)
+
+
+def get_affected_incidents(op_code: str, incidents_list: list[dict]) -> list[dict]:
+    '''Returns a list of incidents that have the operator code in their dictionary'''
+    affected_incidents = []
+    for incident in incidents_list:
+        if op_code in incident['operator_codes']:
+            affected_incidents.append(incident)
+    return [incident for incident in incidents_list if op_code in incident['operator_codes']]
 
 
 if __name__ == "__main__":
@@ -122,7 +162,19 @@ if __name__ == "__main__":
     sns = get_sns_client()
     topics = get_topics_arns_from_aws(sns)
     subscribed_topics = filter_topics(FILTER_TOPICS_BY, topics)
-    operator_list = extract_operator_to_dictionary(subscribed_topics)
-    logging.info("\nTrain operators in AWS: %s\n", [
-                 tag['operator'] for tag in operator_list])
-    print(operator_list)
+    operator_arn_list = extract_operator_to_dictionary(subscribed_topics)
+    operator_list = [
+        topic['shortcode'] for topic in operator_arn_list]
+    logging.info("\nTrain operators in AWS: %s\n", operator_list)
+
+    incidents = get_data_from_json()
+
+    for operator in operator_arn_list:
+        operator['incidents'] = get_affected_incidents(
+            operator['shortcode'], incidents)
+
+        if len(operator['incidents']) == 0:
+            continue
+        response = publish_list_to_topic(
+            sns, operator['TopicArn'], operator['shortcode'], operator['incidents'])
+        print(response)
