d7ded2bbfd7669601854fb29189a2311120656a6
Fariha <trainee.fariha.choudhury@sigmalabs.co.uk>
2024-07-24 12:24:02 +0100

    rds

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,41 +8,41 @@ provider "aws" {
 }
 
 # --------------- RDS: 
-# resource "aws_db_instance" "c11-railway-tracker-db" {
-#     allocated_storage            = 10
-#     db_name                      = "railwaytrackerdb"
-#     identifier                   = "c11-railway-tracker-db"
-#     engine                       = "postgres"
-#     engine_version               = "16.1"
-#     instance_class               = "db.t3.micro"
-#     publicly_accessible          = true
-#     performance_insights_enabled = false
-#     skip_final_snapshot          = true
-#     db_subnet_group_name         = "c11-public-subnet-group"
-#     vpc_security_group_ids       = [aws_security_group.c11-railway-tracker-RDS-sg-terrafrom.id]
-#     username                     = var.DB_USERNAME
-#     password                     = var.DB_PASSWORD
-# }
+resource "aws_db_instance" "c11-railway-tracker-db" {
+    allocated_storage            = 10
+    db_name                      = "railwaytrackerdb"
+    identifier                   = "c11-railway-tracker-db"
+    engine                       = "postgres"
+    engine_version               = "16.1"
+    instance_class               = "db.t3.micro"
+    publicly_accessible          = true
+    performance_insights_enabled = false
+    skip_final_snapshot          = true
+    db_subnet_group_name         = "c11-public-subnet-group"
+    vpc_security_group_ids       = [aws_security_group.c11-railway-tracker-RDS-sg-terrafrom.id]
+    username                     = var.DB_USERNAME
+    password                     = var.DB_PASSWORD
+}
 
-# resource "aws_security_group" "c11-railway-tracker-RDS-sg-terrafrom" {
-#     name = "c11-railway-tracker-RDS-sg-terrafrom"
-#     description = "Security group for connecting to RDS database"
-#     vpc_id = data.aws_vpc.c11-vpc.id
+resource "aws_security_group" "c11-railway-tracker-RDS-sg-terrafrom" {
+    name = "c11-railway-tracker-RDS-sg-terrafrom"
+    description = "Security group for connecting to RDS database"
+    vpc_id = data.aws_vpc.c11-vpc.id
 
-#     egress {
-#         from_port        = 0
-#         to_port          = 0
-#         protocol         = "-1"
-#         cidr_blocks      = ["0.0.0.0/0"]
-#     }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-#     ingress {
-#         from_port = 5432
-#         to_port = 5432
-#         protocol = "tcp"
-#         cidr_blocks      = ["0.0.0.0/0"]
-#     }
-# }
+    ingress {
+        from_port = 5432
+        to_port = 5432
+        protocol = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
 
 data "aws_vpc" "c11-vpc" {
     id = var.C11_VPC
