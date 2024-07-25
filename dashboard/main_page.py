a7d8dee5367d94c36c9935f71c2cade4da3fc3c6
fm1psy <freddymateo1@gmail.com>
2024-07-25 22:49:00 +0100

    make improvements to the dashboard included a logo for the dashboard header organisation done on the statistics collected, including columns and headers

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -9,6 +9,9 @@ TIME_RANGE_OPTIONS = ["last day", "last week", "All time"]
 TIME_RANGE_OPTIONS_DICT = {
     "last day": '24 hours', "last week": '128 hours', "All time": None}
 
+LOGO_URL = "../diagrams/train_logo.png"
+LOGO_ICON_URL = "../diagrams/train_logo.png"
+
 
 def display_total_delays(date_range: str) -> alt.Chart:
     """"""
@@ -25,7 +28,7 @@ def display_total_delays(date_range: str) -> alt.Chart:
 
 def display_train_cancellation_proportion() -> alt.Chart:
     """"""
-    ...
+    return alt.Chart()
     # pie = alt.Chart(train_cancellation_percentages_df).mark_arc(innerRadius=50, radius=100).encode(
     #     theta=alt.Theta("cancellation_percentage").stack(True),
     #     color=alt.Color("station_name").legend(None)
@@ -42,11 +45,10 @@ def display_train_cancellation_percentage() -> alt.Chart:
         get_trains_cancelled_per_station_percentage())
     train_cancellation_percentages_df["cancellation_percentage"] = train_cancellation_percentages_df["cancellation_percentage"].astype(
         float)
-    pie = alt.Chart(train_cancellation_percentages_df).mark_bar().encode(
+    return alt.Chart(train_cancellation_percentages_df).mark_bar().encode(
         x=alt.X("cancellation_percentage", title="% of trains cancelled"),
         y=alt.Y("station_name", title="Station Name", sort="x")
     )
-    return pie
 
 
 def display_average_delay_all() -> alt.Chart:
@@ -56,15 +58,10 @@ def display_average_delay_all() -> alt.Chart:
         float)
     average_delay_df["avg_departure_delay_minutes"] = average_delay_df["avg_departure_delay_minutes"].astype(
         float)
-    arrival = alt.Chart(average_delay_df).mark_bar().encode(
-        x=alt.X("station_name"),
-        y=alt.Y("avg_arrival_delay_minutes")
+    return alt.Chart(average_delay_df).mark_bar().encode(
+        y=alt.Y("station_name", title="Station Name", sort="x"),
+        x=alt.X("avg_arrival_delay_minutes", title="Average delay in Minutes")
     )
-    departure = alt.Chart(average_delay_df).mark_bar().encode(
-        x=alt.X("station_name"),
-        y=alt.Y("avg_departure_delay_minutes")
-    )
-    return arrival+departure
 
 
 def display_average_delays_over_a_minute() -> alt.Chart:
@@ -73,9 +70,15 @@ def display_average_delays_over_a_minute() -> alt.Chart:
     avg_delay_long_df["avg_overall_delay_minutes"] = avg_delay_long_df["avg_overall_delay_minutes"].astype(
         float)
     return alt.Chart(avg_delay_long_df).mark_bar().encode(
-        x=alt.X("station_name"),
-        y=alt.Y("avg_overall_delay_minutes", title="OVER A MINUTE")
+        y=alt.Y("station_name", title="Station Name", sort="x"),
+        x=alt.X("avg_overall_delay_minutes",
+                title="Average delay (over a minute long)")
     )
+
+
+def display_rolling_average_delay() -> alt.Chart:
+    """"""
+    ...
 
 
 def deploy_station_dashboard():
@@ -93,36 +96,47 @@ def deploy_station_dashboard():
     date_range = TIME_RANGE_OPTIONS_DICT[date_range]
     st.write(get_station_with_highest_delay(date_range))
 
-    # TODO: DO something with sigma radio
+    # TODO: change statistics collected
     sigma = st.radio("label", ["arrival", "departure", "sum total"])
-    # st.altair_chart(display_train_cancellations())
-    bar_charts = st.columns(2, gap="small")
+
+    proportions_column = st.columns(2, gap="large")
+    with proportions_column[0]:
+        st.altair_chart(display_train_cancellation_percentage())
+    with proportions_column[1]:
+        ...
+        # st.altair_chart(display_train_cancellation_proportion())
+
+    bar_charts = st.columns(2, gap="large")
     with bar_charts[0]:
-        st.header("Total Delay in minutes by Station")
+        st.subheader("Total Delay in minutes by Station")
         st.altair_chart(display_total_delays(date_range))
     with bar_charts[1]:
-        st.header("Percentage of Trains cancelled per station")
+        st.subheader("Percentage of Trains cancelled per station")
         st.altair_chart(display_train_cancellation_percentage())
 
+    st.header("Place Holder header")
     delays = st.columns(2, gap="large")
     with delays[0]:
-
+        st.subheader("Delay all")
         st.altair_chart(display_average_delay_all())
     with delays[1]:
-
+        st.subheader("Delay over")
         st.altair_chart(display_average_delays_over_a_minute())
+
+    # st.altair_chart(display_rolling_average_delay())
 
 
 def deploy_operator_dashboard():
     """"""
     ...
-# TODO: check people can't dox emails
 
 
 def deploy_home_page():
     """This displays the main page of the dashboard."""
-    st.set_page_config(layout="wide")
+    st.set_page_config(
+        layout="wide", page_title="Railway Tracker", page_icon=LOGO_URL)
 
+    st.logo(LOGO_URL, icon_image=LOGO_ICON_URL)
     show_pages(
         [
             Page("main_page.py", "Home", "🏠"),
