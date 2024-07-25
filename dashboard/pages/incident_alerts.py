2f6f6c756eace9bece850d679b21bb1e7e0f7ceb
fm1psy <freddymateo1@gmail.com>
2024-07-25 12:10:22 +0100

    refactor to respond to requests made by PR comments added more detailed doc strings made changes to raise pylint score

--- dashboard/pages/incident_alerts.py
+++ dashboard/pages/incident_alerts.py
@@ -3,6 +3,7 @@ creates a page in the dashboard that allows users to subscribe to an alert syste
 that notifies users if an operator they are interested in is suffering from an incident.
 """
 import re
+import logging
 from os import environ
 
 from boto3 import client
@@ -27,7 +28,7 @@ def get_db_connection() -> connection | None:
             port=environ['DB_PORT']
         )
     except Exception as e:  # pylint: disable=broad-exception-caught
-        print(e)
+        logging.error(e)
         return None
 
 
@@ -36,7 +37,7 @@ def get_db_cursor(conn: connection) -> cursor | None:
     try:
         return conn.cursor(cursor_factory=RealDictCursor)
     except Exception as e:  # pylint: disable=broad-exception-caught
-        st.write(e)
+        logging.error(e)
         return None
 
 
@@ -49,38 +50,40 @@ def get_sns_client():
             aws_secret_access_key=environ['AWS_SECRET_KEY'],
         )
     except Exception as e:  # pylint: disable=broad-exception-caught
-        st.write(e)
+        logging.error(e)
         return None
 
 
-def does_topic_arn_exist(client, topic_arn: str) -> bool:
-    """"""
+def does_topic_arn_exist(sns_client, topic_arn: str) -> bool:
+    """bool test to determine whether a topic_arn exists in the SNS client"""
     try:
-        response = client.get_topic_attributes(
+        response = sns_client.get_topic_attributes(
             TopicArn=topic_arn
         )
         return response is not None
-    except:
+    except Exception:  # pylint: disable=broad-exception-caught
         return False
 
 
-def create_topic(client, operator_code: str) -> None:
-    """"""
-    client.create_topic(
+def create_topic(sns_client, operator_code: str) -> None:
+    """create a topic based on the operator code a user wishes to subscribe to
+    this is done if the topic does not exist in the client already"""
+    sns_client.create_topic(
         Name=f"c11-trainwreck-{operator_code}"
     )
 
 
-def subscribe_to_topic(client, topic_arn: str, protocol: str, endpoint: str) -> None:
-    """"""
+def subscribe_to_topic(sns_client, topic_arn: str, protocol: str, endpoint: str) -> None:
+    """subscribe to a topic using an SNS client, given a protocol and appropriate endpoint"""
     try:
-        return client.subscribe(TopicArn=topic_arn, Protocol=protocol, Endpoint=endpoint)
-    except:
+        return sns_client.subscribe(TopicArn=topic_arn, Protocol=protocol, Endpoint=endpoint)
+    except Exception:  # pylint: disable=broad-exception-caught
+        logging.error("Failed to subscribe user to a train alert")
         return None
 
 
 def get_list_of_operators() -> list[str]:
-    """"""
+    """return a list of all operators available to subscribe to"""
     conn = get_db_connection()
 
     with get_db_cursor(conn) as curs:
@@ -92,33 +95,37 @@ def get_list_of_operators() -> list[str]:
 
 
 def is_email_valid(email: str) -> bool:
-    """return a bool based on whether this is a valid email"""
+    """bool test to determine if this is a valid email"""
+    if not email:
+        return True
     email_pattern = re.compile(r"[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}")
-    return email and re.fullmatch(pattern=email_pattern, string=email) is not None
+    return re.fullmatch(pattern=email_pattern, string=email) is not None
 
 
 def is_phone_no_valid(phone_no: str) -> bool:
-    """"""
-    number_pattern = re.compile(r"0[0-9]{4} [0-9]{6}")
+    """bool test to determine if a phone number is valid"""
+    if not phone_no:
+        return True
+    number_pattern = re.compile(r"\+44 [0-9]{4} [0-9]{3} [0-9]{3}")
     return phone_no and re.fullmatch(pattern=number_pattern, string=phone_no) is not None
 
 
 def get_user_contacts_from_form() -> dict | None:
-    """create a subscription form for users to submit their contact information."""
+    """create a subscription form for users to submit their contact information"""
     with st.form("alert_subscription", clear_on_submit=False, border=True):
         operator = st.selectbox("Select an Operator", get_list_of_operators())
         email = st.text_input("Enter your email")
         phone_no = st.text_input(
-            "Enter your phone number (in the format 0#### ######)")
+            "Enter your phone number (use the following format: +44 XXXX XXX XXX)")
 
         submit = st.form_submit_button()
 
         if operator and submit and (email or phone_no):
             valid_email = is_email_valid(email)
             valid_phone = is_phone_no_valid(phone_no)
-            if (valid_email and not phone_no) or (valid_phone and not email):
-                st.success(f"Form submitted! email: {email}, \
-                           phone: {phone_no}, operator: {operator}")
+            if valid_email and valid_phone:
+                st.success(
+                    f"Form submitted! you are now able to receive alerts for {operator}")
                 operator = operator.split(" ")[0]
                 return {"email": email, "phone": phone_no, "operator": operator}
             if not valid_email:
@@ -132,7 +139,7 @@ def get_user_contacts_from_form() -> dict | None:
 def deploy_alerts_page():
     """This serves as the main code for the alerts page"""
     st.title("Train Alerts")
-    st.write("""Subscribe to an Operator Alert using your phone number, 
+    st.write("""Subscribe to an Operator Alert using your phone number,
              and you will receive a notification when this operator
              is affected by an incident.""")
 
@@ -143,16 +150,17 @@ def deploy_alerts_page():
         sns_client = get_sns_client()
         subscriber_arn = f"{ARN_PREFIX}-{operator_code}"
         if not does_topic_arn_exist(sns_client, subscriber_arn):
-            st.write(f"{subscriber_arn} IS NEW")
             create_topic(sns_client, operator_code)
         if email:
             subscribe_to_topic(
-                client=sns_client, topic_arn=subscriber_arn, protocol="email", endpoint=email)
+                sns_client=sns_client, topic_arn=subscriber_arn, protocol="email", endpoint=email)
         if phone_no:
             subscribe_to_topic(
-                client=sns_client, topic_arn=subscriber_arn, protocol="sms", endpoint=phone_no)
+                sns_client=sns_client, topic_arn=subscriber_arn, protocol="sms", endpoint=phone_no)
 
 
 if __name__ == "__main__":
     load_dotenv()
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - %(levelname)s - %(message)s")
     deploy_alerts_page()
